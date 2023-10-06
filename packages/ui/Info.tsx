09b806d5bd90ae5d49a0987f018fb152828bf8d2
dmpierre <pdaixmoreux@gmail.com>
2023-10-06 14:48:54 +0200

    feat: add warning

--- packages/ui/Info.tsx
+++ packages/ui/Info.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-export const Info = ({ text }: { text: string }) => {
-  return <div>{text}</div>;
+export const Info = ({ text, className }: { text: string, className: string }) => {
+  return <div className={className}>{text}</div>;
 };
